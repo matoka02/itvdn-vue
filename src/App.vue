b4195ebd4220863e4398ad303b9cadbb60d80997
matoka02 <a.m.nigomedzyanova@gmail.com>
2024-01-18 21:37:07 +0200

    update lesson-02

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,54 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!-- <h1 class="hello">Hello, {{ name }}</h1>
+
+  <div class="container">
+    <button @click="flag = !flag">Click me!</button>
+    <h2 v-if="flag">Hello!</h2>
+    <h2 v-else>Bye-bye</h2>
+  </div>
+  <div class="container-2">
+    <button @click="count = count + 1">Click +1</button>
+    <p>{{ count }}</p>
+  </div>
+
+  <div class="container-3">
+    <input type="text" v-model="text" />
+    <p>{{ text }}</p>
+  </div> -->
+  <Cards/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { 
+  defineComponent, 
+  // ref, 
+} from "vue";
+import Cards from "@/components/Cards";
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+export default defineComponent({
+  name: "App",
+  // components: { },
+  // setup() {
+  //   const name = "Tessa";
+  //   const flag = ref(true);
+  //   const count = ref(0);
+  //   const text = ref("");
+  //   return { name, flag, count, text };
+  // },
+
+  components: { Cards },
+  setup(){
+    return {};
   }
-}
+});
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+/* .hello {
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+} */
+
+ul {
+  list-style: none;
 }
 </style>
---
+++ src/components/Card.vue
@@ -0,0 +1,25 @@
+<template>
+  <ul class="card" v-for="card in arrayCard" :key="card.name">
+    <li>
+      <img :src="card.src" :alt="card.alt" style="width: 100px;">
+      {{ card.name }} - {{ card.age }}
+    </li>
+  </ul>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: 'Card',
+  props: {
+    arrayCard: Array
+  },
+  setup() {
+
+    return {};
+  }
+})
+</script>
+
+<style scoped></style>
