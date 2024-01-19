86a7e654774a0f4e0fee982df73b08624418805f
matoka02 <a.m.nigomedzyanova@gmail.com>
2024-01-19 19:56:53 +0200

    update lesson-05

---
+++ src/components/ChildComponent.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="child-container">
+    <!-- option API -->
+    <!-- <h2><slot name="title"></slot></h2>
+    <div><slot name="content"></slot></div> -->
+
+    <!-- composition API -->
+    <h2><slot name="title"></slot></h2>
+    <div><slot></slot></div>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: 'ChildComponent',
+  
+  // composition API
+  setup(){
+    return{};
+  }
+})
+</script>
+
+<style></style>
---
+++ src/components/ParentComponent.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="parent-container">
+    <h2>Parent Component</h2>
+    <ChildComponent>
+      <!-- option API -->
+      <!-- <template v-slot:content>Child Component Content. Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident
+        sequi ut animi blanditiis molestiae, quibusdam eius, pariatur nulla dolor reiciendis sit id mollitia aspernatur
+        sed quos quaerat, temporibus cum itaque!</template>
+      <template v-slot:title>Child Component Title</template> -->
+
+      <!-- composition API -->
+      <template v-slot:title>Child Component Title</template>
+      <p>Child Component Content. Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident
+        sequi ut animi blanditiis molestiae, quibusdam eius, pariatur nulla dolor reiciendis sit id mollitia aspernatur
+        sed quos quaerat, temporibus cum itaque!</p>
+
+    </ChildComponent>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+import ChildComponent from '@/components/ChildComponent.vue';
+
+export default defineComponent({
+  name: 'ParentComponent',
+  components: { ChildComponent },
+  setup(){
+    return {};
+  }
+})
+</script>
+
+<style></style>
