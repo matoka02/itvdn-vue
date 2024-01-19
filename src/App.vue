9e11d5bb59facc0509fec286a7aee40dc38da8d3
matoka02 <a.m.nigomedzyanova@gmail.com>
2024-01-19 18:08:09 +0200

    update lesson-04

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,56 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!-- <input type="number" v-model="counter">
+  <Counter :counter="counter" /> -->
+
+  <div class="main-block">
+    <button @click="showModal">Show modal window</button>
+    <br><br><br>
+    <label for="modalTitle">Title:</label>
+    <input type="text" id="modalTitle" v-model="modalTitle">
+    <br><br>
+    <label for="modalMessage">Message:</label>
+    <input type="text" id="modalMessage" v-model="modalMessage">
+    <br><br>
+    <Modal v-if="isModalOpen" :modal-title="modalTitle" :modal-message="modalMessage" @close="closeModal" />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import {
+  defineComponent,
+  ref,
+} from "vue";
+// import Counter from '@/components/Counter';
+import Modal from '@/components/Modal';
 
-export default {
+export default defineComponent({
   name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  // components: { Counter },
+  // setup() {
+  //   const counter = ref(0);
+  //   return { counter };
+  // },
+
+  components: { Modal },
+  setup() {
+    const isModalOpen = ref(false);
+    const modalTitle = ref('');
+    const modalMessage = ref('');
+
+    function showModal() {
+      isModalOpen.value = true;
+      // modalTitle.value = 'Default title';
+      // modalMessage.value = 'Default message';
+    }
+    function closeModal() {
+      isModalOpen.value = false;
+      modalTitle.value = '';
+      modalMessage.value = '';
+    }
+    return { isModalOpen, modalTitle, modalMessage, showModal, closeModal };
+  },
+
+})
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
---
+++ src/components/Counter.vue
@@ -0,0 +1,20 @@
+<template>
+  <h2>Counter (child component) {{ counter }}</h2>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: 'Counter',
+  // components: {},
+  props: {
+    counter: Number,
+  },
+  setup() {
+    return {};
+  }
+})
+</script>
+
+<style></style>
