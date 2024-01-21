dd2fb77807907348f77a98c917e106315915211c
matoka02 <a.m.nigomedzyanova@gmail.com>
2024-01-21 21:33:56 +0200

    update lesson-08

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,52 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div v-if="!loading">
+    <Gallery :limit="perPage" :page="currentPage" />
+    <Pagination :on-change-page="handleChangePage" :currentPage="currentPage" :page-count="pageCount" />
+  </div>
+  <Loader v-else />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { defineComponent, onMounted, ref, computed } from 'vue';
 
-export default {
+import { fetchAllPhotos } from '@/api/fetchData';
+import Loader from '@/components/Loader.vue';
+import Gallery from './components/Gallery';
+import Pagination from './components/Pagination';
+
+export default defineComponent({
   name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  components: { Loader, Gallery, Pagination },
+  setup() {
+    const currentPage = ref(1);
+    const perPage = ref(15);
+    const photosCount = ref(0);
+    const loading = ref(true);
+
+    const handleChangePage = (page) => {
+      currentPage.value = page
+    };
+
+    const pageCount = computed(() => {
+      return Math.ceil(photosCount.value / perPage.value)
+    });
+
+    onMounted(() => {
+      fetchAllPhotos().then((data) => {
+        photosCount.value = data.length;
+        loading.value=false;
+      }).catch((error) => {
+        console.error(error);
+        loading.value=false;
+      })
+    });
+    return { currentPage, perPage, photosCount, loading, handleChangePage, pageCount }
+  },
+})
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+  font-family: 'Lexend', sans-serif;
 }
 </style>
